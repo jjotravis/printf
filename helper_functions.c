a8104ecf46caa56ada0e3b7cdfdec6edd4c4e904
OptimusJc <optimus.prime.jc@gmail.com>
2023-07-26 01:43:21 +0300

    Add helper functions

--- helper_functions.c
+++ helper_functions.c
@@ -23,3 +23,39 @@ int hex_check(int num, char c)
 
 	return (0);
 }
+
+/**
+ * base_len - return the character len of converted integer
+ * @number: number to be convert
+ * @base: base to be converted to
+ * Return: char count
+ */
+int base_len(size_t number, int base)
+{
+	size_t i;
+
+	for (i = 0; number > 0; i++)
+		number = number / base;
+	return (i);
+}
+
+/**
+ * get_num_len - get the len of the number passed
+ * @number: number
+ * Return: len of the number
+ */
+int get_num_len(size_t num)
+{
+	int len;
+
+	len = 0;
+	if (num == 0)
+		return (1);
+
+	while (num > 0)
+	{
+		len = len + 1;
+		num = num / 10;
+	}
+	return (len);
+}
